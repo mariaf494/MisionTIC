6b8bc004be392d1d4737f576db14719ba3432b12
mariaf494 <mariaf494@gmail.com>
2021-05-02 21:36:51 -0500

    Update encuestas.py

--- encuestas.py
+++ encuestas.py
@@ -186,7 +186,7 @@ def pag_encuestas(col_preguntas, columna_unica):
 
         answers = set(df[pregunta])
         st.write(answers)
-        st.write(len(set(dudas).intersection(answers)) >= len(answers))
+        st.write(len(set(dudas).intersection(answers)))
 
         if len(set(satisfaction).intersection(answers)) >= len(answers):
             cat_order = satisfaction
