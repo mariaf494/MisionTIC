2387cb693f993ad5d3791f5d160f4bbc7275c768
rmanes22 <rmanes@uninorte.edu.co>
2021-05-11 19:39:54 -0500

    cambios

--- encuestas.py
+++ encuestas.py
@@ -337,7 +337,7 @@ def main():
         pag_encuestas(4, columna_unica, "Misión_TIC.xlsx")
 
     elif pag == "Encuesta docentes":
-    	pag_docentes(2, columna_unica, "Docentes_sem1")
+    	pag_docentes(2, columna_unica, "Docentes_sem1.xlsx")
 
     elif pag == "Habilidades en programación":
         pag_habilidades(3, columna_unica,
