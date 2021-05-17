be7212d04cc752489db165c42f465ff73cd96af4
rmanes22 <rmanes@uninorte.edu.co>
2021-05-17 08:45:21 -0500

    AJUSTE

--- encuestas.py
+++ encuestas.py
@@ -200,8 +200,8 @@ def pag_encuestas(col_preguntas, columna_unica, file):
         yes_no = ["Sí", "No"]
         dificultad = ["No tuvo dificultades", "Muy bajo",
                       "Bajo", "Intermedio", "Alto", "Muy alto"]
-        dudas = ["Sobre la metodología", "Comprensión de las temáticas",
-                 "Asociado a los retos", "Instrucciones recibidas", "No tuvo dificultades"]
+        dudas = ["No tuvo dificultades", "Sobre la metodología", "Comprensión de las temáticas",
+                 "Asociado a los retos", "Instrucciones recibidas"]
         tema = ["Manejo del tiempo", "Plan de vida", "Manejo del estrés y la ansiedad",
                 "Estrategias para trabajar en grupo", "Establecimiento y cumplimiento de objetivos"]
         tiempo = ["1 hora", "2 horas", "3 horas",
